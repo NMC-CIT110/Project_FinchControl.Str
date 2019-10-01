72c1429826027f974141ce8abf88e68133d27388
johnvelis <john@johnvelis.com>
2019-10-01 11:09:23 -0400

    initial push

--- Project_FinchControl/Program.cs
+++ Project_FinchControl/Program.cs
@@ -1,17 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FinchAPI;
 
 namespace Project_FinchControl
 {
+
+    // **************************************************
+    //
+    // Title: Finch Control
+    // Description: 
+    // Application Type: Console
+    // Author: 
+    // Dated Created: 
+    // Last Modified: 
+    //
+    // **************************************************
+
     class Program
     {
         static void Main(string[] args)
         {
+            DisplayWelcomeScreen();
+
+
+            DisplayClosingScreen();
         }
 
+        /// <summary>
+        /// display welcome screen
+        /// </summary>
+        static void DisplayWelcomeScreen()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tFinch Control");
+            Console.WriteLine();
 
+            DisplayContinuePrompt();
+        }
+
+        /// <summary>
+        /// display closing screen
+        /// </summary>
+        static void DisplayClosingScreen()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tThank you for using Finch Control!");
+            Console.WriteLine();
+
+            DisplayContinuePrompt();
+        }
+
+        #region HELPER METHODS
+
+        /// <summary>
+        /// display continue prompt
+        /// </summary>
+        static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// display screen header
+        /// </summary>
+        static void DisplayScreenHeader(string headerText)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t" + headerText);
+            Console.WriteLine();
+        }
+
+        #endregion
     }
 }
